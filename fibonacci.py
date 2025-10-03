57e7d0f959f4c8ff0809f95145d0868c059b95ee
Naysa Samuel <naysa@uoguelph.ca>
2025-10-03 12:32:35 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -4,22 +4,22 @@
 # TODO: (Read detailed instructions in the Readme file)
 # Prompt the user for the number of terms.
 while True:
-  user_input = input("Enter the number of terms you'd like to see:")
+    user_input = input("Enter the number of terms you'd like to see:")
   # Validate that the input is a positive integer 
-  if user_input is digit(): 
+    if user_input.isdigit(): 
      num_terms = int(user_input)
-    if user_input >0:
+     if num_terms >0:
           break
-       else:
-          print("Invalid input. Positive integers only.") 
+     else:
+           print("Invalid input. Positive integers only.") 
     else:
         print("Invalid input. Positive integers only.")
    # Use a for loop to print the Fibonacci sequence up to that many terms.
- a = 0
- b = 1
- for i in range (num_terms): 
+a = 0
+b = 1
+for i in range (num_terms): 
   print(a, end=' ')
-  a = b
-  b = a + b
+  a, b = b, a + b
+  #I'm going to need a lot more practice for indentation :')
 
 
